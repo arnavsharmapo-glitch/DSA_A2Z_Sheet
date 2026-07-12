45277b77abbd10c280913bec16959d137841b1b5
Arnav Sharma <arnav.sharma.po@gmail.com>
2026-07-12 22:45:23 +0530

    Set up folder structure and add Input/Output solution

---
+++ 01-Basics/Input_Output.cpp
@@ -0,0 +1,14 @@
+// Problem 1: Input Output
+// Link: https://takeuforward.org/plus/dsa/problems/input-output
+// Difficulty: Easy
+// Approach: Basic cin/cout usage
+
+#include<iostream>
+using namespace std;
+
+int main() {
+    int x;
+    cin >> x;
+    cout << "Value of x : " << x;
+    return 0;
+}
